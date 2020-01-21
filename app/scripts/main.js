5bb1b0aef7dd268fae3033ec97ab12a7ab55f19d
dodgerfox <limmeco@gmail.com>
2020-01-22 00:15:07 +0300

    refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,7 +7,7 @@ window.onload = () => {
 
 
 const forum = {
-  data: function () {
+  data () {
     fetch('assets/data/articles.json').then( (response) => {
       
       response.json().then((result) => {
@@ -19,25 +19,20 @@ const forum = {
       })
     })
   },
-  init: function () {
+  init () {
     this.data();
     this.helpers();
   },
-  catcheDOM: function () {
+  catcheDOM () {
     this.input = document.querySelector('.search-input input[type="search"]');
     this.list = document.querySelector('.timeline-body');
     this.detail = document.querySelector('.article-wrap');
   },
-  bindEvents: function () {
+  bindEvents () {
     this.input.addEventListener('keyup', () => { this.render() })
-    this.posts.forEach((post) => {
-      
-      post.addEventListener('click', () => {
-        this.openArticle(post)
-      })
-    })
+    this.posts.forEach((post) => {post.addEventListener('click', () => { this.openArticle(post) }) })
   },
-  helpers: function () {
+  helpers () {
     $(".timeline").mCustomScrollbar({
       scrollInertia: 350,
       autoHideScrollbar: true,
@@ -49,8 +44,10 @@ const forum = {
       mouseWheel:{ scrollAmount: 200 }
     }); 
   },
-  openArticle: function (post) {
+  openArticle (post) {
     this.detail.innerHTML = '';
+    this.posts.forEach((post) => {post.classList.remove('active');})
+    post.classList.toggle('active');
     this.articles.forEach((article, id) => {
       if (article.id == post.getAttribute('data-id')){
         const header = 
@@ -81,10 +78,9 @@ const forum = {
     })
     
   },
-  render: function () {
+  render () {
     const articlesList =  this.articles.filter((article) => {
-      const value = this.input.value || '';
-      return article.title.toUpperCase().indexOf(value.toUpperCase()) > -1;
+      return article.title.toUpperCase().indexOf(this.input.value.toUpperCase()) > -1;
     });
     
     this.list.innerHTML = '';
@@ -93,12 +89,29 @@ const forum = {
       
       articlesList.forEach( ( article ) => {
         
-        const elem = `<article class="post" data-id="` + article.id + `"> <p class="post-title">` + article.title + `</p>`,
-        cats = '<div class="category"><div class="category-item snowboard"></div><div class="category-item ski"></div><div class="category-item skateboard"></div></div>',
-        info = `<div class="info"><div class="info-wrap"><p class="info__tag">` + article.tags + `</p><p class="info__date">` + article.date + `</p></div><div class="info-wrap"><a class="info__flame" href="#"><img src="assets/images/icon_flame.svg" class="mCS_img_loaded"></a><a class="info__user" href="#" style="background-image: url('` + article.avatar + `');"></a></div></div></article>`,
-        allElems = elem + cats + info;
+        const element = 
+              `<article class="post" data-id="` + article.id + `"> 
+                <p class="post-title">` + article.title + `</p>
+                <div class="category">
+                  <div class="category-item snowboard"></div>
+                  <div class="category-item ski"></div>
+                  <div class="category-item skateboard"></div>
+                </div>',
+                <div class="info">
+                  <div class="info-wrap">
+                    <p class="info__tag">` + article.tags + `</p>
+                    <p class="info__date">` + article.date + `</p>
+                  </div>
+                  <div class="info-wrap">
+                    <a class="info__flame" href="#">
+                      <img src="assets/images/icon_flame.svg" class="mCS_img_loaded">
+                    </a>
+                    <a class="info__user" href="#" style="background-image: url('` + article.avatar + `');"></a>
+                  </div>
+                </div>
+              </article>`;
         
-        this.list.insertAdjacentHTML("beforeEnd", allElems);
+        this.list.insertAdjacentHTML("beforeEnd", element);
       })
     }else{
       this.list.insertAdjacentHTML("beforeEnd", "<p>Нетъ</p>");
@@ -116,51 +129,3 @@ const panel = () => {
     panel.classList.toggle('active');
   })
 }
-
-// let articles;
-
-// fetch('assets/data/articles.json').then( (response) => {
-  
-  //   response.json().then((result) => {
-    //     articles = result;
-    //     timeline(articles)
-    //   })
-
-// })
-
-
-
-// let timeline = () => {
-//   const input = document.querySelector('.search-input input[type="search"]'),
-//         posts = articles,
-//         list = document.querySelector('.timeline-body');
-  
-//   let renderList = () => {
-//     const articlesList =  posts.filter(function(article) {
-//       const value = input.value || '';      
-//       return article.title.toUpperCase().indexOf(value.toUpperCase()) > -1;
-//     });
-    
-//     list.innerHTML = '';
- 
-//     if (articlesList.length > 0) {
-      
-//       articlesList.forEach( ( article ) => {
-
-//         const elem = `<article class="post"> <p class="post-title">` + article.title + `</p>`,
-//               cats = '<div class="category"><div class="category-item snowboard"></div><div class="category-item ski"></div><div class="category-item skateboard"></div></div>',
-//               info = `<div class="info"><div class="info-wrap"><p class="info__tag">` + article.tags + `</p><p class="info__date">` + article.date + `</p></div><div class="info-wrap"><a class="info__flame" href="#"><img src="assets/images/icon_flame.svg" class="mCS_img_loaded"></a><a class="info__user" href="#" style="background-image: url('` + article.avatar + `');"></a></div></div></article>`,
-//               allElems = elem + cats + info;
-              
-//         list.insertAdjacentHTML("beforeEnd", allElems);
-//       })
-//     }else{
-//       list.insertAdjacentHTML("beforeEnd", "<p>Нетъ</p>");
-//     }
-//   }
-
-//   renderList()
-
-//   input.addEventListener('keyup', () => { renderList() })
-  
-// }
